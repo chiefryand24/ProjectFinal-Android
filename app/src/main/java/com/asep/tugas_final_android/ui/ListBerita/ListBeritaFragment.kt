101a9eb9a1e8a5bc7e63199ba9aa15fc7c76908a
ahmadfma <83245934+chiefryand24@users.noreply.github.com>
2021-12-11 09:22:27 +0800

    membuat fragment ListBeritaFragment

---
+++ app/src/main/java/com/asep/tugas_final_android/ui/ListBerita/ListBeritaFragment.kt
@@ -0,0 +1,26 @@
+package com.asep.tugas_final_android.ui.ListBerita
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.asep.tugas_final_android.R
+
+class ListBeritaFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_berita, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = ListBeritaFragment()
+    }
+}

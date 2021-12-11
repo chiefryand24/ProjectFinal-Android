81ab1789b456988e62168bd1c7c00ea9cf24b12c
ahmadfma <83245934+chiefryand24@users.noreply.github.com>
2021-12-11 09:42:49 +0800

    membuat class ListBeritaViewModel

---
+++ app/src/main/java/com/asep/tugas_final_android/ui/ListBerita/ListBeritaViewModel.kt
@@ -0,0 +1,25 @@
+package com.asep.tugas_final_android.ui.ListBerita
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.asep.tugas_final_android.model.DataNews
+import com.asep.tugas_final_android.network.RetrofitClient
+import retrofit2.Response
+import java.lang.Exception
+
+class ListBeritaViewModel: ViewModel() {
+    private val TAG = "ListBeritaViewModel"
+
+    suspend fun getBerita() {
+        var response: Response<DataNews>? = null
+        try {
+            response = RetrofitClient.instance.getNews()
+            if(response.isSuccessful) {
+
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "${e.message}")
+        }
+    }
+
+}
